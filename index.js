b4b3ed5bcf4835dfe7dd127aace566360423093f
Josh <joshua.rowan.2021@gmail.com>
2024-01-31 10:05:36 -0600

    done for day to do comments in the js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-const specials = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let specials = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
 const password1El = document.getElementById("password1-el")
 const myRangeEl = document.getElementById("my-range")
@@ -8,9 +8,9 @@ const password2El = document.getElementById("password2-el")
 const rangeEl = document.getElementById("range-el")
 const numberToggle = document.getElementById("check-numbers")
 
-
-// try looking up adding event listener for checkboxes
-// or getting value or status of checkbox from js
+//Okay. Now, use an empty password array, then push requested values in to select from, a getRandomCharacter Function
+// instead of different functions. Push the appropriate values into the array, then pull values to return array. 
+// once i have that reliably creating passwords, I can work on validating if the array has all requested values or not.
 
 rangeEl.textContent = " " + myRangeEl.value
 
@@ -37,6 +37,13 @@ function getRandomSpecial() {
     return randomSpecial
 }
 
+const lettersAndNumbers = [getRandomLetter(), getRandomNumber()]
+
+function getRandomNumberOrLetter() {
+    let randomNumberOrLetter = lettersAndNumbers[Math.floor(Math.random() * randomNumberOrLetter.length)]
+    return randomNumberOrLetter
+}
+
 console.log(getRandomNumber())
 console.log(getRandomSpecial())
 
@@ -47,8 +54,17 @@ function newPassword() {
             password += getRandomLetter(i)
         }
             return password
-    } else {
-        return "PasswordWithNumbersSelected"
+    } else if (numberToggle.checked) {
+        for (num=0; num<numbers.length; num++)
+        letters.push(numbers[num])
+        console.log(letters)
+        for (let i = 0; i < myRangeEl.value; i++) {
+            password += getRandomLetter(i)
+        }
+            return password
+    } else
+    {
+        return "Ooops"
     }
     }
 
