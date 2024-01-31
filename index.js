1f14b551780ce7beb3e4df2ddf0fa55c48d9cffb
Josh <joshua.rowan.2021@gmail.com>
2024-01-31 09:32:11 -0600

    functions to get random number and special characters created and working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const specials = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
@@ -22,18 +22,34 @@ myRangeEl.addEventListener("input", (event) => {
 let password1 = ""
 let password2 = ""
 
-function getRandomCharacter() {
-    let randomCharacter = characters[Math.floor(Math.random() * characters.length)]
-    return randomCharacter
+function getRandomLetter() {
+    let randomLetter = letters[Math.floor(Math.random() * letters.length)]
+    return randomLetter
 }
 
+function getRandomNumber() {
+    let randomNumber = numbers[Math.floor(Math.random() * numbers.length)]
+    return randomNumber
+}
+
+function getRandomSpecial() {
+    let randomSpecial = specials[Math.floor(Math.random() * specials.length)]
+    return randomSpecial
+}
+
+console.log(getRandomNumber())
+console.log(getRandomSpecial())
+
 function newPassword() {
-    console.log(numberToggle.checked)
     let password = ""
-    for (let i = 0; i < myRangeEl.value; i++) {
-        password += getRandomCharacter(i)
+    if (!numberToggle.checked) {
+        for (let i = 0; i < myRangeEl.value; i++) {
+            password += getRandomLetter(i)
+        }
+            return password
+    } else {
+        return "PasswordWithNumbersSelected"
     }
-        return password
     }
 
 function getRandomPassword() {
