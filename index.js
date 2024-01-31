a1d4bdf17cecac0c86ab11c3a3ff5582ecbcf69d
Josh <joshua.rowan.2021@gmail.com>
2024-01-31 09:04:55 -0600

    numberToggle.checked succesfully gets checkbox status. Separated password character to 3 arrays - letters, numbers, specials

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const specials = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
-let password1El = document.getElementById("password1-el")
-let password2El = document.getElementById("password2-el")
-let myRangeEl = document.getElementById("my-range")
-let rangeEl = document.getElementById("range-el")
-let numberOption = document.getElementById("check-numbers")
+const password1El = document.getElementById("password1-el")
+const myRangeEl = document.getElementById("my-range")
+const password2El = document.getElementById("password2-el")
+const rangeEl = document.getElementById("range-el")
+const numberToggle = document.getElementById("check-numbers")
 
 
+// try looking up adding event listener for checkboxes
+// or getting value or status of checkbox from js
 
 rangeEl.textContent = " " + myRangeEl.value
 
@@ -25,6 +28,7 @@ function getRandomCharacter() {
 }
 
 function newPassword() {
+    console.log(numberToggle.checked)
     let password = ""
     for (let i = 0; i < myRangeEl.value; i++) {
         password += getRandomCharacter(i)
